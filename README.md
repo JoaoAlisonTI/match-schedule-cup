5dff7befe778576b53bb19a301294239617c4cdd
JoaoAlisonTI <joaoalison.ti@gmail.com>
2023-01-25 08:58:11 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+<h1 align="center"> Match Schedule</br>World Cup 2022 </h1>
+
+<p align="center">
+Desenvolvido durante o evento NLW - COPA, promovido pela Rocketseat 🚀⚡. <br/>
+</p>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="Calendário de Jogos" src=".github/" width="100%">
+</p>
+
+## ⚙️👨‍💻 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+- JavaScript
+- Git e Github
+- Figma
+
+## 💻 Projeto
+
+O Match Schedule foi desenvolvido com o tema da Copa do Mundo 2022 ⚽. Tem como objetivo mostrar quais partidas iriam ocorrer durante a semana e com os respectivos horários. Os cards das partidas são adicionados pelo Javascript. Bem, o código pode servir de base para outros projetos envolvendo calendário de jogos, eventos ou para outras ideias e necessidades que surgirem.
+
+- [Acesse o projeto finalizado, online ☁️]()
+
+## 🖼️ Layout
+
+Você pode visualizar o layout do projeto através [DESSE LINK](). É necessário ter conta no [Figma](https://figma.com) para acessá-lo.
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT.
+
+---
+
+<strong>2022</strong>
---
+++ main.js
@@ -0,0 +1,63 @@
+function createGame(player1, hour, player2) {
+  return `
+    <li>
+      <img src="./nation_icons/icon=${player1}.svg" alt="Bandeira do ${player1}" />
+      <strong>${hour}</strong>
+      <img src="./nation_icons/icon=${player2}.svg" alt="Bandeira da ${player2}" />
+    </li>
+  `
+}
+let delay = -0.4
+function createCard(date, day, games) {
+  delay = delay + 0.4
+  return `
+    <div class="card" style="animation-delay: ${delay}s">
+      <h2>${date} <span>${day}</span></h2>
+      <ul>
+        ${games}
+      </ul>
+    </div>
+  `
+}
+
+document.querySelector("#cards").innerHTML =
+  createCard("20/11", "domingo", createGame("qatar", "13:00", "ecuador")) +
+  createCard(
+    "21/11",
+    "segunda",
+    createGame("england", "10:00", "iran") +
+      createGame("senegal", "13:00", "netherlands") +
+      createGame("united states", "16:00", "wales")
+  ) +
+  createCard(
+    "22/11",
+    "terça",
+    createGame("argentina", "07:00", "saudi arabia") +
+      createGame("denmark", "10:00", "tunisia") +
+      createGame("mexico", "13:00", "poland") +
+      createGame("france", "16:00", "australia")
+  ) +
+  createCard(
+    "23/11",
+    "quarta",
+    createGame("morocco", "07:00", "croatia") +
+      createGame("germany", "10:00", "japan") +
+      createGame("spain", "13:00", "costa rica") +
+      createGame("belgium", "16:00", "canada")
+  ) +
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("switzerland", "07:00", "cameroon") +
+      createGame("uruguay", "10:00", "south korea") +
+      createGame("portugal", "13:00", "ghana") +
+      createGame("brazil", "16:00", "serbia")
+  ) +
+  createCard(
+    "25/11",
+    "sexta",
+    createGame("wales", "07:00", "iran") +
+      createGame("qatar", "10:00", "senegal") +
+      createGame("netherlands", "13:00", "ecuador") +
+      createGame("england", "16:00", "united states")
+  )
